29b2fabd49d33414c2070d4d5d6f2b30c5784d46
rubi kang <rubi0624@nate.com>
2022-02-10 18:55:39 +0900

    Initial Commit

---
+++ QuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGenerator
+//
+//  Created by 강루비 on 2022/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
